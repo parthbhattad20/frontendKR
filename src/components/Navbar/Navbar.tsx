fecc41f274943d5187a21b27126bce7a7a522830
Parth Bhattad <bhattadparth20@gmail.com>
2024-04-03 10:21:13 +0530

    changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,102 +1,85 @@
-import { Link } from "react-router-dom";
-import { Button } from "../ui/button";
-import { useAuth } from "@/context/AuthContext";
-import { useToast } from "../ui/use-toast";
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
 import logo from "../../images/KloudRakhsa.png";
 
-function Navbar() {
-    const auth = useAuth();
-    const { toast } = useToast();
+const Navbar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-    console.log(auth);
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
 
-    function handleLogout() {
-        auth?.logout();
-        toast({
-            title: `Logged out ${auth?.user?.name}`,
-        });
-    }
-
-    const list = [
-        {
-            name: "About",
-            link: "/about",
-        },
-        {
-            name: auth?.user ? "Support" : "Help",
-            link: auth?.user ? "/support" : "/help",
-        },
-        {
-            name: "Contact",
-            link: "/contact",
-        },
-        {
-            name:  "Plans" ,
-            link:  "/payment" 
-        },
-      
-    ];
     return (
-        <nav className="mx-auto py-4 px-4 flex justify-between max-w-7xl items-center">
-            <div>
-                <Link to="/">
-                    <img
-                        src={logo}
-                        alt="Website Logo"
-                        className="scroll-m-20 text-xl font-semibold tracking-tight"
-                        style={{ width: "25%", height: "25%" }} // Reduce the size by 40%
-                    />
-                </Link>
-            </div>
-            <div className="flex gap-2 items-center">
-                {list.map((item) => {
-                    if (item.name === "Login") {
-                        return (
-                            <Button
-                                className="font-bold"
-                                variant="secondary"
-                                key={item.name}
-                                asChild
+        <nav className="sticky top-0 z-50 bg-white shadow border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a
+                    href="/"
+                    className="flex items-center space-x-3 rtl:space-x-reverse"
+                >
+                    <img src={logo} className="h-10" alt="Flowbite Logo" />
+                </a>
+                <button
+                    onClick={toggleNavbar}
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-solid-bg"
+                    aria-expanded={isOpen ? "true" : "false"}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <FaBars className="w-5 h-5" />
+                </button>
+                <div
+                    className={`w-full md:flex md:w-auto ${
+                        isOpen ? "" : "hidden"
+                    }`}
+                    id="navbar-solid-bg"
+                >
+                    <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700 items-center">
+                        <li>
+                            <a
+                                href="/"
+                                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent items-center"
+                                aria-current="page"
                             >
-                                <Link to={item.link}>{item.name}</Link>
-                            </Button>
-                        );
-                    } else if (item.name === "Get Started") {
-                        return (
-                            <Button
-                                className="font-bold"
-                                key={item.name}
-                                asChild
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/about"
+                                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >
-                                <Link to={item.link}>{item.name}</Link>
-                            </Button>
-                        );
-                    } else if (item.name === "Logout") {
-                        return (
-                            <Button
-                                onClick={handleLogout}
-                                className="font-bold"
-                                key={item.name}
-                                asChild
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/payment"
+                                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >
-                                <Link to={item.link}>{item.name}</Link>
-                            </Button>
-                        );
-                    }
-                    return (
-                        <Button
-                            className="font-bold"
-                            variant="ghost"
-                            key={item.name}
-                            asChild
-                        >
-                            <Link to={item.link}>{item.name}</Link>
-                        </Button>
-                    );
-                })}
+                                Plans
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/contact"
+                                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                            >
+                                Contact
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/help"
+                                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                            >
+                                Help
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
-}
+};
 
 export default Navbar;
